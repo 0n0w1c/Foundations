cb6a8bb9459fac13bcbb86b1fad8956a5442fe20
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-09-07 09:46:47 -0500

    Version 2.8.3

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,11 +1,12 @@
 for _, tile in pairs(data.raw["tile"]) do
-    if tile.minable then
-        tile.minable.mining_time = settings.startup["Foundations-mining-time"].value
-        tile.minable.hardness = nil
-
-        if settings.startup["Foundations-clean-sweep"].value then
-            tile.decorative_removal_probability = 1
+    if settings.startup["Foundations-mining-time"].value > 0 then
+        if tile.minable then
+            tile.minable.mining_time = settings.startup["Foundations-mining-time"].value
+            tile.minable.hardness = nil
         end
     end
 
+    if settings.startup["Foundations-clean-sweep"].value then
+        tile.decorative_removal_probability = 1
+    end
 end
