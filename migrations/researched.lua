db11c3f36c48567ff6517563958ebf22ae413341
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-09-19 17:49:35 -0500

    Version 2.10.1

--- migrations/researched.lua
+++ migrations/researched.lua
@@ -1,12 +1,22 @@
 require("constants")
 
 for _, force in pairs(game.forces) do
+    local technologies = force.technologies
     local recipes = force.recipes
+    local researched = false
+
+    if technologies then
+        if game.active_mods["IndustrialRevolution3"] then
+            researched = technologies["ir-concrete-2"].researched
+        else
+            researched = technologies["concrete"].researched
+        end
+    end
 
     if recipes then
         for _, color in pairs(COLORS) do
-            if recipes[color.name.."-refined-concrete"] and recipes["refined-concrete"] then
-                recipes[color.name.."-refined-concrete"].enabled = recipes["refined-concrete"].enabled
+            if recipes[color.name .. "-refined-concrete"] then
+                recipes[color.name .. "-refined-concrete"].enabled = researched
             end
         end
     end
